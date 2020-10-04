9764aa782b5b232f6d7d56f77a5943323f4af8bd
Matias Vazquez-Levi <50473168+matiasvlevi@users.noreply.github.com>
2020-10-04 10:23:45 -0400

    update

---
+++ public/func.js
@@ -0,0 +1,212 @@
+let batches = [];
+let batchNb = 6;
+for (let  i= 0; i < batchNb; i++) {
+  batches[i] = [];
+}
+let img1;
+let count = 0;
+let index = 0;
+let dataset = [];
+let labels = ["airplane","automobile","bird","cat","deer","dog","frog","horse","ship","truck"];
+
+async function loadfile(file, offset) {
+    let r = await fetch(file);
+    let data = await r.arrayBuffer();
+    return new Uint8Array(data).slice(offset);
+}
+let guess = "";
+function findBiggest(arr) {
+  let best = 0;
+  let bestIndex = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= best) {
+      best = arr[i];
+      bestIndex = i;
+    }
+  }
+  return bestIndex;
+}
+function LoadCIFAR(callback) {
+    let ready = 0;
+    loadfile('CIFAR/data_batch_1.bin',0)
+        .then(data => {
+            batches[0] = data;
+            ready++;
+            if (ready == 6) {
+              console.log("ready");
+              formatBatches();
+            }
+        })
+        loadfile('CIFAR/data_batch_2.bin',0)
+            .then(data => {
+                batches[1] = data;
+                ready++;
+                if (ready == 6) {
+                  console.log("ready");
+                  formatBatches();
+                }
+            })
+            loadfile('CIFAR/data_batch_3.bin',0)
+                .then(data => {
+                    batches[2] = data;
+                    ready++;
+                    if (ready == 6) {
+                      console.log("ready");
+                      formatBatches();
+                    }
+                })
+                loadfile('CIFAR/data_batch_4.bin',0)
+                    .then(data => {
+                        batches[3] = data;
+                        ready++;
+                        if (ready == 6) {
+                          console.log("ready");
+                          formatBatches();
+                        }
+                    })
+                    loadfile('CIFAR/data_batch_5.bin',0)
+                        .then(data => {
+                            batches[4] = data;
+                            ready++;
+                            if (ready == 6) {
+                              console.log("ready");
+                              formatBatches();
+                            }
+                        })
+                        loadfile('CIFAR/test_batch.bin',0)
+                            .then(data => {
+                                batches[5] = data;
+                                if (ready == 6) {
+                                  console.log("ready");
+                                  formatBatches();
+                                }
+                            })
+
+
+
+
+}
+function printImgArray(img) {
+  let size = 8;
+  let x = 0;
+  let y = 200;
+  let w = sqrt(img.length);
+  for (let i = 0; i < 32; i++) {
+    for (let j = 0; j < 32; j++) {
+      let col = img[(j*w)+i];
+
+      noStroke();
+      let r = map(col[0],0,1,0,255);
+      let g = map(col[1],0,1,0,255);
+      let b = map(col[2],0,1,0,255);
+      fill(r,g,b);
+      rect((i*size)+x,(j*size)+y,size,size);
+    }
+  }
+}
+function uarrayToData(batch,index) {
+  let imgIndex = (index*3073);
+  let arr = [];
+
+
+  let trainbatch = batches[batch];
+  let labelIndex = trainbatch[imgIndex];
+  let a = trainbatch.slice(imgIndex+1,imgIndex+3073);
+  let inputs = [];
+  for (let i = 0; i < a.length;i++) {
+    inputs[i] = map(a[i],0,255,0,1);
+  }
+
+  let data = {
+    inputs: inputs,
+    target: makelabel(labelIndex)
+  }
+  return data;
+}
+function test(batch) {
+  let sum = 0;
+  for (let i =0; i < 10;i++) {
+    nn.feedForward(batch[i].inputs);
+    guess = labels[findBiggest(nn.outs)];
+    for (let j = 0; j < nn.outs.length; j++) {
+      sum +=1 + batch[i].target[j]-nn.outs[j];
+    }
+    result = sum/nn.outs.length;
+
+  }
+
+
+  return result/10;
+
+}
+function makelabel(x) {
+  let arr = [];
+  for (let i =0; i < 10; i++) {
+    if (i == x) {
+      arr[i] = 1;
+    } else {
+      arr[i]= 0;
+    }
+
+  }
+  return arr;
+}
+function uarrayToImage(batch,index) {
+  let imgIndex = (index*3073);
+  let arr = [];
+  let r = [];
+  let g = [];
+  let b = [];
+  let j = 0;
+  let trainbatch = batches[batch];
+  let labelIndex = trainbatch[imgIndex];
+  //console.log(labels[labelIndex])
+  for (let i = imgIndex+1; i < imgIndex+1025;i++) {
+
+    r[j] = trainbatch[i];
+    j++
+  }
+  j=0;
+  for (let i = imgIndex+1025; i < imgIndex+1025+1024;i++) {
+    g[j] = trainbatch[i];
+    j++
+  }
+
+  j=0;
+  for (let i = imgIndex+1025+1024; i < imgIndex+1025+1024+1024;i++) {
+    b[j] = trainbatch[i];
+    j++
+  }
+  for (let i = 0; i < 1024; i++) {
+    let rc = map(r[i],0,255,0,1);
+    let gc = map(g[i],0,255,0,1);
+    let bc = map(b[i],0,255,0,1);
+    arr[i] = [rc,gc,bc];
+  }
+  return arr;
+}
+function formatBatches() {
+  dataset = setbatches();
+}
+function setbatches() {
+  let data_set = [];
+
+  for (let b = 0; b < 1;b++) {
+    let batch = [];
+
+    for (let i = 0; i < 10000; i++) {
+      batch[i] = uarrayToData(b,i);
+
+    }
+
+
+
+    data_set[b] = batch;
+    console.log("finished batch: " + b);
+  }
+
+
+  //console.log(dataset);
+  //downloadObjectAsJson(dataset[0], "data1")
+  return data_set;
+}
--- public/sketch.js
+++ public/sketch.js
@@ -1,153 +1,92 @@
-let batches = [];
-let batchNb = 6;
-for (let  i= 0; i < batchNb; i++) {
-  batches[i] = [];
-}
-let test_batch = [];
-let labels = ["airplane","automobile","bird","cat","deer","dog","frog","horse","ship","truck"];
-async function loadfile(file, offset) {
-    let r = await fetch(file);
-    let data = await r.arrayBuffer();
-    return new Uint8Array(data).slice(offset);
-}
-function LoadCIFAR(callback) {
-    loadfile('CIFAR/data_batch_1.bin',0)
-        .then(data => {
-            batches[0] = data;
-        })
-        loadfile('CIFAR/data_batch_2.bin',0)
-            .then(data => {
-                batches[1] = data;
-            })
-            loadfile('CIFAR/data_batch_3.bin',0)
-                .then(data => {
-                    batches[2] = data;
-                })
-                loadfile('CIFAR/data_batch_4.bin',0)
-                    .then(data => {
-                        batches[3] = data;
-                    })
-                    loadfile('CIFAR/data_batch_5.bin',0)
-                        .then(data => {
-                            batches[4] = data;
-                        })
-                        loadfile('CIFAR/test_batch.bin',0)
-                            .then(data => {
-                                batches[5] = data;
-                            })
-}
-function printImgArray(img) {
-  let size = 8;
-  let x = 0;
-  let y = 0;
-  let w = sqrt(img.length);
-  for (let i = 0; i < 32; i++) {
-    for (let j = 0; j < 32; j++) {
-      let col = img[(j*w)+i];
-
-      noStroke();
-      let r = map(col[0],0,1,0,255);
-      let g = map(col[1],0,1,0,255);
-      let b = map(col[2],0,1,0,255);
-      fill(r,g,b);
-      rect((i*size)+x,(j*size)+y,size,size);
-    }
-  }
-}
-function uarrayToData(batch,index) {
-  let imgIndex = (index*3073);
-  let arr = [];
-
-  let j = 0;
-  let trainbatch = batches[batch];
-  let labelIndex = trainbatch[imgIndex];
-  let a = trainbatch.slice(imgIndex+1,imgIndex+3073);
-  let inputs = [];
-  for (let i = 0; i < a.length;i++) {
-    inputs[i] = map(a[i],0,255,0,1);
-  }
-
-  let data = {
-    inputs: inputs,
-    target: labelIndex
-  }
-  return data;
-}
-function uarrayToImage(batch,index) {
-  let imgIndex = (index*3073);
-  let arr = [];
-  let r = [];
-  let g = [];
-  let b = [];
-  let j = 0;
-  let trainbatch = batches[batch];
-  let labelIndex = trainbatch[imgIndex];
-  //console.log(labels[labelIndex])
-  for (let i = imgIndex+1; i < imgIndex+1025;i++) {
-
-    r[j] = trainbatch[i];
-    j++
-  }
-  j=0;
-  for (let i = imgIndex+1025; i < imgIndex+1025+1024;i++) {
-    g[j] = trainbatch[i];
-    j++
-  }
-
-  j=0;
-  for (let i = imgIndex+1025+1024; i < imgIndex+1025+1024+1024;i++) {
-    b[j] = trainbatch[i];
-    j++
-  }
-  for (let i = 0; i < 1024; i++) {
-    let rc = map(r[i],0,255,0,1);
-    let gc = map(g[i],0,255,0,1);
-    let bc = map(b[i],0,255,0,1);
-    arr[i] = [rc,gc,bc];
-  }
-  return arr;
-}
-let img1;
-function downloadObjectAsJson(exportObj, exportName){
-
-  var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(exportObj);
-  var downloadAnchorNode = document.createElement('a');
-  downloadAnchorNode.setAttribute("href",     dataStr);
-  downloadAnchorNode.setAttribute("download", exportName + ".json");
-  document.body.appendChild(downloadAnchorNode); // required for firefox
-  downloadAnchorNode.click();
-  downloadAnchorNode.remove();
-}
-
-function formatBatches() {
-  let dataset = [];
-
-  for (let b = 0; b < 5;b++) {
-    let batch = [];
-
-    for (let i = 0; i < 10000; i++) {
-      batch[i] = uarrayToData(b,i);
-
-    }
 
 
-
-    dataset[b] = batch;
-    console.log("finished batch: " + b);
-  }
-
-
-  //console.log(dataset);
-  //downloadObjectAsJson(dataset[0], "data1")
-  return dataset;
+let nn;
+let nng;
+let losses = [];
+let accuracies = [];
+function preload() {
+  LoadCIFAR();
 }
 function setup() {
-  LoadCIFAR();
+
   createCanvas(600,600);
+
 }
-let count = 0;
-let index = 0;
+function initiateNN() {
+  nn = new Dann(3072,10);
+
+  nn.addHiddenLayer(64,leakyReLU);
+  nn.addHiddenLayer(32,leakyReLU);
+  nn.addHiddenLayer(32,leakyReLU);
+  nn.addHiddenLayer(16,leakyReLU);
+  nn.makeWeights();
+  nn.activation(nn.Layers.length,sigmoid)
+  nn.lr = 0.000003
+  nn.log();
+  nng = new Graph(0,0,600,200);
+  nng.addValue(losses,color(0,150,255),"loss");
+  nng.addValue(accuracies,color(255,150,0),"accuracy");
+}
+let time = 0;
+let d_index = 0;
+function display() {
+  if (nng !== undefined) {
+    nng.render();
+    printImgArray(uarrayToImage(0,d_index));
+    nn.feedForward(dataset[0][d_index].inputs);
+    guess = labels[findBiggest(nn.outs)];
+    console.log(nn.outs)
+    fill(255);
+    text(guess + "/   confidence: "+nn.outs[findBiggest(nn.outs)],320,220);
+    if (time > 250) {
+      if (d_index > 10) {
+        d_index = 0;
+      } else {
+        d_index++;
+      }
+      time = 0;
+    } else {
+      time++;
+    }
+
+
+  }
+
+}
 function draw() {
   background(51);
+  display();
+}
+
+let currentIndex = 0;
+let currentBatch = 0;
+
+function train(minibatch) {
+  for (let m = 0; m < minibatch;m++) {
+    let accsum = 0;
+    let losssum = 0;
+    for (let i = 0; i < 100; i++) {
+      let data = dataset[currentBatch][i+currentIndex];
+      // console.log(data,currentIndex+i,currentBatch)
+      nn.backpropagate(data.inputs,data.target);
+      accsum += test(dataset[0]);
+      losssum+=nn.loss;
+    }
+    accuracies.push(accsum/100);
+    losses.push(losssum/100);
+    if (currentIndex >= 10000) {
+      currentIndex=0;
+      if (currentBatch >= 5) {
+        currentBatch = 0;
+        epoch++;
+      } else {
+        currentBatch++;
+      }
+
+    } else {
+      currentIndex+=100;
+    }
+
+  }
 
 }
